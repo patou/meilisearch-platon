62c983a9a9907a8a252465c7ab58cd38ba88b4f6
Patrice <patrice@desaintsteban.fr>
2022-04-12 00:03:33 +0200

    Initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,23 @@
 import { Component } from '@angular/core';
+import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
+import { RefinementListItem } from 'instantsearch.js/es/connectors/refinement-list/connectRefinementList';
 
+const searchClient = instantMeiliSearch(
+  'http://localhost:7700',
+  'lQOoufhq58b38cd83c47e5dc1545fd38ad7ccae1f7cb08e72d2d74159350baf62bb9de8e'
+)
+
+const ONE_MONTH = 30 * 24 * 60 * 60
+const LABELS : { [key : string]: string } = {
+  'COMPANY': 'Company',
+  'REMOTE': 'Remote',
+  'PARTNER': 'Partner',
+  'NONE': 'None',
+  'ZENIKA': 'Zenika',
+  'ONSITE' : 'On site',
+  'PUBLIC': 'Public',
+  'OTHER': 'Other',
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +25,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'meilisearch-platon';
+  config = {
+    indexName: 'sessions',
+    searchClient,
+  }
+  dateMenu = [
+    { label: 'All' },
+    { end: new Date().getTime() / 1000, label: 'Past' },
+    { start: new Date().getTime() / 1000, label: 'Futur' },
+    { start: new Date().getTime() / 1000 - ONE_MONTH, end: new Date().getTime() / 1000 + ONE_MONTH, label: 'Current' },
+  ]
+  labels = LABELS
+
+  transformItems(items : RefinementListItem[]) : RefinementListItem[] {
+    if (items)
+      return items.map(item => ({
+        ...item,
+        label: LABELS[item.label] || item.label,
+        highlighted: LABELS[item.label] || item.label,
+      }));
+    return items;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgAisModule } from 'angular-instantsearch';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgAisModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
